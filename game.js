6ba4189f79d8517104ae0904b3dc289c52c0eb97
roman-cossackov <roman.delarge@gmail.com>
2024-01-25 19:59:27 +0300

    character and enemies added

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
-import { getRandomInt, createRoom, createPassageH, createPassageV } from "./utils.js";
+import {
+	getRandomInt,
+	createRoom,
+	createPassage,
+	createGameObject,
+} from "./utils.js";
 
 export class Game {
 	init() {
+		// Map generation
 		let field = document.getElementsByClassName("field")[0];
 		field.innerHTML = "";
 		for (let i = 0; i < 40 * 24; i++) {
@@ -9,35 +15,62 @@ export class Game {
 			wall.className = "tile tileW";
 			field.appendChild(wall);
 		}
-	}
-	generateGameMap() {
+
+		// Filling the map with a wall
 		const gameMap = new Array(24)
 			.fill()
 			.map(() => new Array(40).fill("wall"));
-        
-        const numRooms = getRandomInt(5, 10)
-        const numPassagesH = getRandomInt(3, 5)
-        const numPassagesV = getRandomInt(3, 5)
-        for (let i = 0; i < numRooms; i++) {
-            createRoom(gameMap)
-        }
-        for (let i = 0; i < numPassagesH; i++) {
-            createPassageH(gameMap)
-        }
-        for (let i = 0; i < numPassagesV; i++) {
-            createPassageV(gameMap)
-        }
 
-        let field = document.getElementsByClassName("field")[0];
-        let tiles = field.querySelectorAll('div');
-        for (let r = 0; r < 24; r++) {
-            for (let c = 0; c < 40; c++) {
-                if (gameMap[r][c] === 'ground') {
-                    const index = 40 * r + c
-                    tiles[index].className = "tile"
-                }
-            }
-        }
+		// Generating rooms, passges and game objects
+		const numRooms = getRandomInt(5, 10);
+		const numPassagesH = getRandomInt(3, 5);
+		const numPassagesV = getRandomInt(3, 5);
+		const numFlasks = 10;
+		const numSwords = 2;
+		const numEnemies = 10;
 
+		for (let i = 0; i < numRooms; i++) {
+			createRoom(gameMap);
+		}
+		for (let i = 0; i < numPassagesH; i++) {
+			createPassage(gameMap, "horizontal");
+		}
+		for (let i = 0; i < numPassagesV; i++) {
+			createPassage(gameMap, "vertical");
+		}
+		for (let i = 0; i < numFlasks; i++) {
+			createGameObject(gameMap, "flask");
+		}
+		for (let i = 0; i < numSwords; i++) {
+			createGameObject(gameMap, "sword");
+		}
+		for (let i = 0; i < numEnemies; i++) {
+			createGameObject(gameMap, "enemy");
+		}
+		const [charY, charX] = createGameObject(gameMap, "character");
+		console.log(charY, charX);
+
+		// Rendering of objects
+		const renderMap = () => {
+			let tiles = field.querySelectorAll("div");
+			for (let r = 0; r < 24; r++) {
+				for (let c = 0; c < 40; c++) {
+					const index = 40 * r + c;
+					if (gameMap[r][c] === "ground") {
+						tiles[index].className = "tile";
+					} else if (gameMap[r][c] === "flask") {
+						tiles[index].className = "tile tileHP";
+					} else if (gameMap[r][c] === "sword") {
+						tiles[index].className = "tile tileSW";
+					} else if (gameMap[r][c] === "character") {
+						tiles[index].className = "tile tileP";
+					} else if (gameMap[r][c] === "enemy") {
+						tiles[index].className = "tile tileE";
+					}
+				}
+			}
+		};
+
+        renderMap()
 	}
 }
--- utils.js
+++ utils.js
@@ -8,25 +8,42 @@ export const createRoom = (gameMap) => {
 	const startX = getRandomInt(0, 40 - roomWidth - 1);
 	const startY = getRandomInt(0, 24 - roomHeight - 1);
 
-    for (let y = startY; y < startY + roomHeight; y++) {
-        for (let x = startX; x < startX + roomWidth; x++) {
-            gameMap[y][x] = "ground";
-        }
-    }
+	for (let y = startY; y < startY + roomHeight; y++) {
+		for (let x = startX; x < startX + roomWidth; x++) {
+			gameMap[y][x] = "ground";
+		}
+	}
 };
 
-export const createPassageH = (gameMap) => {
-    const start = getRandomInt(0, 23);
-    for (let i = 0; i < 40; i++) {
-        gameMap[start][i] = "ground";
-    }
-}
+export const createPassage = (gameMap, direction) => {
+	if (direction === "horizontal") {
+		const start = getRandomInt(0, 23);
+		for (let i = 0; i < 40; i++) {
+			gameMap[start][i] = "ground";
+		}
+	} else if (direction === "vertical") {
+		const start = getRandomInt(0, 39);
+		for (let i = 0; i < 24; i++) {
+			gameMap[i][start] = "ground";
+		}
+	}
+};
 
-export const createPassageV = (gameMap) => {
-    const start = getRandomInt(0, 39);
-    for (let i = 0; i < 24; i++) {
-        gameMap[i][start] = "ground";
-    }
-}
-
-
+export const createGameObject = (gameMap, typeOfObject) => {
+	let [x, y] = [getRandomInt(0, 39), getRandomInt(0, 23)];
+	while (gameMap[y][x] !== "ground") {
+		x = getRandomInt(0, 39);
+		y = getRandomInt(0, 23);
+	}
+    
+	if (typeOfObject === "flask") {
+		gameMap[y][x] = "flask";
+	} else if (typeOfObject === "sword") {
+		gameMap[y][x] = "sword";
+	} else if (typeOfObject === "character") {
+        gameMap[y][x] = "character";
+        return [y, x]
+	} else if (typeOfObject === "enemy") {
+        gameMap[y][x] = "enemy";
+	}
+};
